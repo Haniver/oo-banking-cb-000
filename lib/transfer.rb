ddbe849cacf74ed337131e68b0729c455f6823f4
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-25 16:38:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,4 +10,16 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
+
+  def execute_transaction
+    if !self.valid?
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      self.status = "complete"
+      self.execute_transaction.freeze
+    end
+  end
 end
